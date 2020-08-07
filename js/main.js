f0a2fb7fe89e208d3ed3dda232958bf1190f381f
Erica Cancinos <ericacancinos@hotmail.com>
2020-08-06 22:06:10 -0300

    Agreagndo imagenes y textos
    Dimos animacion a la barra de navegacion, agregamos imagen
    de fondo, y de nuestros clientes, tambien titulos y textos.
    trabajamos con HTML, CSS y un poco de JS.
    
    Linked Issues:
    fixes #1

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() -70;
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+});
+//Efecto scroll
+$('.cf a').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function () {
+                window.location.hash = hash;
+            }
+        );
+    }
+});
